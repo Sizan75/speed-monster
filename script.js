2bebfbc768456755d9e29f62187b17a9ea7305ca
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-14 21:51:04 +0600

    Counter end problem solved

--- script.js
+++ script.js
@@ -112,12 +112,13 @@ const start = () => {
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
-      document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      document.addEventListener('keydown', typeController);
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+      
     }
     count--;
   }, 1000);
